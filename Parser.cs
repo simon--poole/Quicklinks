f35d71080a725ddaf6d66bc61d582a45e4b80b6d
simon--poole <simon__poole@hotmail.com>
2013-12-08 19:59:53 +0000

    Added cases, minor edits to process.
    Definition stage:
    Commands added:
    twitch [twitch_channel]
    ogn|ongamenet|champions
    
    Execution stage:
    Methods added:
    "CMD_hidden" - Executes a hidden command prompt window using data as
    flag.
    Default: - Updated to use try/catch to provide errors on invalid input.

--- Parser.cs
+++ Parser.cs
@@ -16,6 +16,18 @@ namespace QuickLinks
                 string value = "Browser http://reddit.com/r/"+match.Groups[1].Value;
                 return value;
             }
+            match = System.Text.RegularExpressions.Regex.Match(command, @"twitch (.+)");
+            if (match.Groups.Count > 1)
+            {
+                string value = "Browser http://twitch.tv/" + match.Groups[1].Value;
+                return value;
+            }
+            match = System.Text.RegularExpressions.Regex.Match(command, @"(ogn|ongamenet|champions)");
+            if (match.Groups.Count > 1)
+            {
+                string value = "CMD_hidden livestreamer http://twitch.tv/ongamenet mobile_High";
+                return value;
+            }
             match = System.Text.RegularExpressions.Regex.Match(command, @"(\.com|\.co\.uk|www\.)");
             if (Uri.IsWellFormedUriString(command, UriKind.Absolute))
             {
@@ -32,7 +44,7 @@ namespace QuickLinks
         public static void Parse(string command)
         {
             command = checkCommon(command);
-            string[] commands = command.Split(' ');
+            string[] commands = command.Split(new string[]{" "}, 2,System.StringSplitOptions.None);
             string operand = commands[0];
             string data = commands[1];
             switch (operand)
@@ -43,8 +55,25 @@ namespace QuickLinks
                 case "Browser":
                     System.Diagnostics.Process.Start(data);
                     break;
+                case "CMD_hidden":
+                    System.Diagnostics.Process process = new System.Diagnostics.Process();
+                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                    startInfo.FileName = "cmd.exe";
+                    startInfo.Arguments = "/c " + data;
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    break;
                 default:
-                    goto case "Browser";
+                    try
+                    {
+                        System.Diagnostics.Process.Start(data);
+                    }
+                    catch (Exception E)
+                    {
+                        MessageBox.Show(E.Message);
+                    }
+                    break;
             }
 
         }
